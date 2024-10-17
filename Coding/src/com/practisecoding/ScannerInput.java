23b139dd38c1e6e271acb490f0b73d20a5f701fa
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-17 15:42:19 +0530

    reversing the string by taking user input using scanner class

---
+++ Coding/src/com/practisecoding/ScannerInput.java
@@ -0,0 +1,32 @@
+package com.practisecoding;
+
+import java.util.Scanner;
+
+public class ScannerInput {
+	
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+        
+        String reversed = reverseString(input);
+        
+        System.out.println("Reversed String: " + reversed);
+        
+        scanner.close();
+    }
+
+    public static String reverseString(String str) 
+    {
+        char[] charArray = str.toCharArray();
+        String result = "";
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            result += charArray[i];
+        }
+        return result;
+    }
+
+
+}
