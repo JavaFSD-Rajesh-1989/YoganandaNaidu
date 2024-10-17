31ac9d7e46a2aaeb08c1cb2106c6381e4249e914
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-17 21:42:36 +0530

    aggreation example

---
+++ Coding/src/com/practisecoding/Employee.java
@@ -0,0 +1,33 @@
+package com.practisecoding;
+
+public class Employee {
+	
+	
+	int empId; 
+	
+	String name; 
+	
+	Address address;
+
+	public Employee(int empId, String name, Address address) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+	} 
+	
+	void display() {
+		System.out.println(empId+" "+name);
+		System.out.println(address.city+" "+address.state+" "+address.pincode);
+	}
+	
+	
+	static public void main(String []args) {
+		
+		Address address = new Address("Anantapur", "Andhra Pradesh", "515004");
+		Employee emp = new Employee(101, "Yogananda", address);
+		emp.display();
+	}
+	
+
+}
